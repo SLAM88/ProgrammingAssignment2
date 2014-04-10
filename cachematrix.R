f47af1f968638c2f7daab0653078e8426a0b537d
SLAM88 <stanley.a.lam@gmail.com>
2014-04-10 14:08:05 -0700

    Added comments and detailed test scripts

--- cachematrix.R
+++ cachematrix.R
@@ -1,40 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## These functions calcluate the inverse of a matrix, then cache its value.
+## This means that when the inverse is calculated later on,
+## the system can look up the cache value and return that 
+## instead of redoing a calculation it has already done.
+
+## This function takes an NxN matrix as input and generates the matrix inverse. 
+## The invest is then cached  using a list implementation.
  
 makeCacheMatrix <- function(x = matrix()) {
-        s <- NULL
+        s <- NULL                                   ## Variables initialized
         set <- function(y) {
             x <<- y
             s <<- NULL
         }
         get <- function() x
-        setinverse <- function(solve) s <<- solve
+        setinverse <- function(solve) s <<- solve  ## Solve calculates inverse
         getinverse <- function() s
-        list(set = set, get = get,
+        list(set = set, get = get,                 ## Cache list populated
              setinverse = setinverse,
              getinverse = getinverse)
     
 }
 
 
-## Write a short comment describing this function
+## This function returns the cached inverse value if it exists.
+## If it doesn't exist, it calls the earlier function to calculate the inverse.
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
-        s <- x$getinverse()
-        if(!is.null(s)) {
+        s <- x$getinverse()                        ## Checks if cache exists
+        if(!is.null(s)) {                          ## If yes, returns cache
             message("getting cached data")
             return(s)
         }
-        data <- x$get()
+        data <- x$get()                            ## If no, calculates inverse
         s <- solve(data, ...)
         x$setinverse(s)
         s
     }
 
-test <- matrix(c(1,8,3, 11,25,13, 3,6,2), nrow = 3, ncol = 3)
+## End functions.
+## Below are simple test scripts used to check that everything works.
 
+## Generate valid test matrix
+test <- matrix(c(1,8,3, 11,25,13, 3,6,2), nrow = 3, ncol = 3)
 test
-cacheSolve(makeCacheMatrix(cacheSolve(makeCacheMatrix(test))))
+
+## Test each function once to ensure no errors
+test1 <- cacheSolve(makeCacheMatrix(test))
+test1
+
+## Test cacheSolve's ability to return cached inverse
+test2 <- makeCacheMatrix(test)
+cacheSolve(test2)
+cacheSolve(test2)
+
+## Validate inverse calculation is correct by checking whether two calls
+## of each function returns the original matrix
+test3 <- cacheSolve(makeCacheMatrix(cacheSolve(makeCacheMatrix(test))))
+test3
